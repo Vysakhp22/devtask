1f7b824e617118564d80068ba307ab1353397126
Vysakh P <vysakhp17@gmail.com>
2026-07-27 10:54:12 +0530

    feat: add custom exception handling with ApiException and ResourceNotFoundException

---
+++ backend/devtask-api/src/main/java/com/devtask/exception/ApiException.java
@@ -0,0 +1,17 @@
+package com.devtask.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public abstract class ApiException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final HttpStatus httpStatus;
+
+    public ApiException(ErrorCode errorCode, HttpStatus httpStatus, String message) {
+        super(message);
+        this.errorCode = errorCode;
+        this.httpStatus = httpStatus;
+    }
+}
